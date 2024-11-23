2a83534b6ee92c2cb23f214662d0542e2bee4cc5
LE DINH KHANH <137139001+ldkhanh15@users.noreply.github.com>
2024-11-23 21:40:14 +0700

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+    @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    assertThat(true);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
